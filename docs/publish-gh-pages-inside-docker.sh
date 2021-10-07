c59d3de2df70ef15c197707cfaf0e031e2e8d9b0
bojernico <nico.bojer@gmail.com>
2021-10-07 18:34:08 +0200

    changed main to master

--- docs/publish-gh-pages-inside-docker.sh
+++ docs/publish-gh-pages-inside-docker.sh
@@ -24,13 +24,13 @@ git init
 git add .
 git commit -m "$GIT_GLOBAL_USER_NAME - rebuilding gh-pages $(date)"
 
-# rename branch to 'main' if necessary
+# rename branch to 'master' if necessary
 CURRENT_BRANCH=$(git branch --show-current)
 echo "current branch: $CURRENT_BRANCH"
-if [ "$CURRENT_BRANCH" != "main" ]
+if [ "$CURRENT_BRANCH" != "master" ]
 then
-  git branch -M main
-  echo "branch $CURRENT_BRANCH renamed to 'main'"
+  git branch -M master
+  echo "branch $CURRENT_BRANCH renamed to 'master'"
 else
   echo "branch $CURRENT_BRANCH not renamed"
 fi
